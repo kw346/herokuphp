aaafc0826e8d41c513636a2306cba67aea7bec9c
kw346 <20022735@myrp.edu.sg>
2022-07-05 12:24:52 +0800

    forth

--- web/test.php
+++ web/test.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="description" content="Lesson 8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="theme-color" content="#317EFB"/>
     <title>First</title>
 </head>
 <body>
